e6205a86ece68f511450bcc41ece48cec8b8c511
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2023-12-13 17:23:06 +0530

    Add files via upload

---
+++ Array2.cs
@@ -0,0 +1,31 @@
+// In this program we learn how to create an array in different ways.
+
+using System;
+
+namespace array
+{
+    class Array2
+    {
+        static void Main(string[] args)
+        {
+         
+            // create and array and add values later.
+
+            string[] name = new string[4];
+
+            // create an array of four elements and  add values immediately.
+
+            string[] name1 = new string[4] {"piyush", "Vyankatesh", "Asus", "C#"};
+
+            //create an array of elements without specifying the size
+
+            string[] name2 = new string[] {"a", "b", "c"};
+
+            //Create an array of elements 
+
+            string[] name3 = {"b", "g", " h" , "k" , "T"};
+
+        }
+
+    }
+}
