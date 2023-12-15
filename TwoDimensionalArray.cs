10fd41474b77b886b7694a5cb03a8ee811551f45
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2023-12-15 23:58:23 +0530

    Add files via upload

---
+++ TwoDimensionalArray.cs
@@ -0,0 +1,15 @@
+// in this program we create a two dimentional Array.
+
+using System;
+
+namespace array
+{
+    class TwoDimensionalArray
+    {
+        
+        int[,] no = {{11,21,51,101} , { 1,2,3,4 }};
+        // the single Comma(,) indicates that the array is multi dimensional.
+
+        // if the array is three- dimentional then there will be 2 single commas[,,].
+    }
+}
