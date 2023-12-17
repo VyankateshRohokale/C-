85c783658812d31749a495470c731948da82cb55
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2023-12-18 00:03:23 +0530

    Add files via upload

---
+++ Method.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace methods
+{
+
+    class methods
+    {
+
+        static void Display()
+    {
+        Console.WriteLine("Inside Display Method ...");
+    }
+
+        static void Main(string[] args)
+        {
+           Display();
+        }
+
+    }
+}
---
+++ ParametersAndArguments.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace methods
+{
+    class ParametersAndArguments
+    {
+        static void Display(string name)
+        {
+            Console.WriteLine(name + " Patil "  ); 
+        }
+
+
+        static void Main(string[] args)
+        {   
+
+            Display("Piyush");
+            Display("Yash");          
+
+        }
+
+    }
+}
+
