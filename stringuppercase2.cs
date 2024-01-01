eabb6b35be3409ad2ac150a513ab3f6b8320788e
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-01-01 21:09:26 +0530

    Update stringuppercase2.cs

--- stringuppercase2.cs
+++ stringuppercase2.cs
@@ -1,7 +1,7 @@
 // The error after compilation , "Property or indexer 'string.this[int]' cannot be assigned to -- it is read-only," is because we're trying to modify a character in 
 //a string directly, which is not allowed because strings in C# are immutable (cannot be changed).
 
-// this code was written for practice of making lower case to upper case without inbuild function.
+//  this code was written for practice of making lower case to upper case without inbuild function.
 
 
 using System;
